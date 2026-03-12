3e00d51efe5ef73c61feada78517d8f368236cf0
Koala Zub <koalazub@users.noreply.github.com>
2026-03-12 20:58:19 +1100

    Initial Commit

---
+++ stonkwatch-app/stonkwatch_appApp.swift
@@ -0,0 +1,32 @@
+//
+//  stonkwatch_appApp.swift
+//  stonkwatch-app
+//
+//  Created by koalazub on 12/3/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct stonkwatch_appApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ stonkwatch-appTests/stonkwatch_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  stonkwatch_appTests.swift
+//  stonkwatch-appTests
+//
+//  Created by koalazub on 12/3/2026.
+//
+
+import Testing
+
+struct stonkwatch_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
